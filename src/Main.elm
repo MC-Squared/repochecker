823164122928fc913a01ca0d15aee226b9ef4976
Jonathan Miles <jonathan.miles@reinteractive.net>
2019-08-15 10:29:37 +1200

    Adding check for Gemfile.lock

--- src/Main.elm
+++ src/Main.elm
@@ -22,7 +22,9 @@ type Model
 type alias Repo =
     { name: String
     , url: String
+    , gemlock: String
     }
+
 
 init : () -> (Model, Cmd Msg)
 init _ =
@@ -41,6 +43,7 @@ init _ =
 
 type Msg
     = GotRepos (Result Http.Error (List Repo))
+    | GotGemlock (Result Http.Error String)
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
@@ -48,11 +51,18 @@ update msg model =
     GotRepos result ->
         case result of
             Ok repoList ->
-                (Success repoList, Cmd.none)
+                (Success repoList, (gemlockExists repoList))
 
             Err _ ->
                 (Failure, Cmd.none)
 
+    GotGemlock result ->
+        case result of
+            Ok content ->
+                (Success content, Cmd.none)
+
+            Err _ ->
+                (Failure, Cmd.none)
 
 -- SUBSCRIPTIONS
 subscriptions : Model -> Sub Msg
@@ -73,11 +83,14 @@ view model =
 
         Success repoList ->
             repoList
-                |> List.map (\l -> pre [] [ text ("name:" ++ l.name ++ "\n")
-                                          , text ("url:" ++ l.url)
+                |> List.map (\l -> pre [] [ text ("name: " ++ l.name ++ "\n")
+                                          , text ("url: " ++ l.url ++ "\n")
+                                          , text ("Gemlock: " ++ l.url ++ "/Gemfile.lock - Exists? ")
                                           ])
                 |> div []
 
+        -- SuccessLock content ->
+        --         pre [] [ text content ]
 
 -- HTTP
 
@@ -87,6 +100,41 @@ repoDecoder =
 
 repoUrlDecoder : Decoder Repo
 repoUrlDecoder =
-    map2 Repo
+    map3 Repo
         (field "name" string)
         (field "html_url" string)
+        (field "notrealfield" string)
+
+isRepoChecked : Repo -> Bool
+isRepoChecked repo =
+    not (String.isEmpty repo.gemlock)
+    -- case repo.gemlock of
+    --     Just r ->
+    --         True
+
+    --     Nothing ->
+    --         False
+gemlockDecoder : Decoder String
+gemlockDecoder =
+    "Test"
+    -- field "items" (list repoUrlDecoder)
+
+checkRepoVersions : Maybe Repo -> Cmd Msg
+checkRepoVersions repo =
+    case repo of
+        Just r ->
+          Http.get
+              { url = r.url ++ "/Gemfile.lock"
+              , expect = Http.expectString GotGemlock gemlockDecoder
+              }
+
+        Nothing ->
+            Cmd.none
+
+
+gemlockExists : (List Repo) -> Cmd Msg
+gemlockExists repoList =
+    repoList
+    |> List.filter isRepoChecked
+    |> List.head
+    |> checkRepoVersions
